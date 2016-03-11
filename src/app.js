1228d0a09d437ccfd81cde522366f3548a994290
brynjas <brynja13@ru.is>
2016-03-11 19:49:25 +0000

    tablesort

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-angular.module("project3App", ["ngRoute", "ui.bootstrap", "sharedServices"])
+angular.module("project3App", ["ngRoute", "ui.bootstrap","tableSort" ,"sharedServices"])
 .config(function ($routeProvider) {
 	$routeProvider.when("/", {
 		controller: "SellersController",
