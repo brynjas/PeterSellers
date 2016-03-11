4401eb1f23588eb90fb372b24745ace367e087ad
brynjas <brynja13@ru.is>
2016-03-11 19:15:02 +0000

    byrjad a sellers

--- src/components/sellers/SellersController.js
+++ src/components/sellers/SellersController.js
@@ -4,5 +4,8 @@ angular.module("project3App").controller("SellersController",
 function SellersController($scope, AppResource) {
 	// TODO: load data from AppResource! Also, add other methods, such as to
 	// add/update sellers etc.
+	AppResource.getSellers().success(function(sellers) {
+		$scope.sellers = sellers;
+	})
 
 });
