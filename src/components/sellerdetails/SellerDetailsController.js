a98d0ece47698e1876838858aff506e8dc8d7e2a
brynjas <brynja13@ru.is>
2016-03-15 15:39:24 +0000

    THIS SELLER HAS NO PRODUCT er komið

--- src/components/sellerdetails/SellerDetailsController.js
+++ src/components/sellerdetails/SellerDetailsController.js
@@ -6,15 +6,25 @@ function SellerDetailsController($scope, $routeParams, AppResource) {
 	// add/update sellers etc.
 	
 	var id = $routeParams.id;
-	console.log("ID: " + id);
+	//console.log("ID: " + id);
 
 	AppResource.getSellerProducts(parseInt(id)).success(function(products){
-		console.log("PRODUCTS: " + products);
+		//console.log("PRODUCTS: " + products);
 		$scope.products = products;
+		$scope.NoProducts = true;
+
+
+		  if (products.length < 1) {
+		  	console.log( "LENGDIN: " + products.length);
+		  	$scope.NoProducts = false;
+		  }
+
+
+  	
 	});
 
 	AppResource.getSellerDetails(parseInt(id)).success(function(seller){
-		console.log("SELLER: " + seller);
+		//console.log("SELLER: " + seller);
 		$scope.seller = seller;
 	});
 
@@ -30,6 +40,10 @@ function SellerDetailsController($scope, $routeParams, AppResource) {
 
     $scope.activeTab = 'AllProducts.html';  
 
+
+
+ 
+
     $scope.onClick = function(tab) {
       $scope.activeTab = tab.url;
     };
@@ -37,6 +51,8 @@ function SellerDetailsController($scope, $routeParams, AppResource) {
     $scope.isActive = function(tabUrl) {
       return tabUrl === $scope.activeTab;
     };
+
+
 
     
    
